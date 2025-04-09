332fddffc57ca443d4f65420d26c434b61b67b7e
Alina44444 <alina.valiyeva.s@mail.ru>
2025-04-09 21:51:47 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public void print() {
+        System.out.println(this.active + " " + this.status + " " + this.message);
+    }
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error errorOne = new Error();
+        errorOne.print();
+        Error errorTwo = new Error(false, 2, "ErrorTwo");
+        errorTwo.print();
+        Error errorThree = new Error(true, 6, "ErrorSix");
+        errorThree.print();
+    }
+}
